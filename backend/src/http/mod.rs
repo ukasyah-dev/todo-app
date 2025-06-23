3f09e88ee78c04d8068bbc6575712498b626256f
Ukasyah <hi.ukasyah@gmail.com>
2025-06-23 18:26:56 +0700

    serve frontend as static files

--- backend/src/http/mod.rs
+++ backend/src/http/mod.rs
@@ -1,5 +1,6 @@
 use axum::{Json, Router, routing::get};
 use serde::Serialize;
+use tower_http::services::{ServeDir, ServeFile};
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_swagger_ui::SwaggerUi;
@@ -9,11 +10,15 @@ use utoipa_swagger_ui::SwaggerUi;
 struct ApiDoc;
 
 pub fn new_router() -> Router {
+    let serve_dir = ServeDir::new("public").not_found_service(ServeFile::new("public/index.html"));
+
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .route("/api", get(health_check))
         .split_for_parts();
 
-    router.merge(SwaggerUi::new("/api/swagger-ui").url("/api/openapi.json", api))
+    router
+        .merge(SwaggerUi::new("/api/swagger-ui").url("/api/openapi.json", api))
+        .fallback_service(serve_dir)
 }
 
 #[derive(Serialize)]
